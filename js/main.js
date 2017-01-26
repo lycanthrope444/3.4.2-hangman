b8a21a88d792e7d7e0052573f61219c35706cb4c
Nathan <nathanstarwalt@gmail.com>
2017-01-26 17:59:28 -0500

    Word randomizer returns 3 or more characters

--- js/main.js
+++ js/main.js
@@ -4,20 +4,40 @@
 var guessword ="";
 
 var turns = 8;
-
+//sets up buttons
+var submitGuess = document.querySelector(".button");
+submitGuess.addEventListener("click", function(){
+  console.log("Submit Clicked");
+});
+//creates random word
 function wordRandomizer(){
   return Math.floor(Math.random()*(100));
 }
 
+
+//used to make sure word is greater than 3
+function cullWords(possibleWord){
+  if (possibleWord.length > 2){
+    return true;
+  }
+}
+
+function wordChecker(){
+  guessword = commonWords[wordRandomizer()];
+  while (cullWords(guessword) !== true){
+    guessword = commonWords[wordRandomizer()];
+  }
+  return guessword;
+}
+
+wordChecker();
+console.log(guessword);
+//used to start/reset the game
 function gameStart(){
 
 }
 
-console.log(wordRandomizer());
 
-guessword = commonWords[wordRandomizer()];
-
-console.log(guessword);
 
 
 
